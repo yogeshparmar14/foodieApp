287d77f2fa32ac61154c93f92f1efa280fcb6a8d
Yogesh Parmar <mr.yoraj@gmail.com>
2022-06-29 09:44:25 +0530

    error solved

--- src/db/connectiondb.js
+++ src/db/connectiondb.js
@@ -1,16 +1,37 @@
 import mongoose from "mongoose";
 
-const connectDb = async (DATABASE_URL_LOCAL, DATABASE_URL_ATLAS)=>{
-    try {
-        const DB_OPTION = {
+// const connectDb = async (DATABASE_URL_LOCAL, DATABASE_URL_ATLAS)=>{
+//     try {
+//          const DB_OPTION = {
+//             dbName:"restaurantapp"
+//         }
+//        let DATABASE_URL = DATABASE_URL_LOCAL;
+//        if(process.env.NODE_ENV === "production"){
+//         DATABASE_URL = DATABASE_URL_ATLAS
+//        }
+
+//        await mongoose.connect(DATABASE_URL, DB_OPTION )
+//        console.log("connected successfully")
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+const connectDb = async (DATABASE_URL_ATLAS)=>{
+    try { 
+         const DB_OPTION = {
             dbName:"restaurantapp"
         }
-       let DATABASE_URL = DATABASE_URL_LOCAL;
-       if(process.env.NODE_ENV === "production"){
-        DATABASE_URL = DATABASE_URL_ATLAS
-       }
+       
+        let DATABASE_URL = DATABASE_URL_ATLAS
+       
 
-       await mongoose.connect(DATABASE_URL, DB_OPTION )
+       await mongoose.connect(DATABASE_URL, DB_OPTION,{
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    
+    } )
        console.log("connected successfully")
 
     } catch (error) {
