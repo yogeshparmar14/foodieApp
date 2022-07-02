a3354d76fac082383fb35e26ef119ac08c21bf91
twointo2 <twointo2@gmail.com>
2022-07-03 00:36:01 +0530

    fixed image upload issue

--- src/modules/common/uploadFile.js
+++ src/modules/common/uploadFile.js
@@ -1,51 +1,62 @@
-require("dotenv").config()
-const {s3Uploadv2} = require('./s3Service.js')
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
+const multerS3 = require("multer-s3");
+const { S3 } = require("aws-sdk");
+const uuid = require("uuid").v4;
 const router = express.Router();
 
-const storage = multer.memoryStorage()
-
-const fileFilter = (req,file,cb)=>{
-    if(file.mimetype.split("/")[0]==="image"){
-        cb(null,true)
-    }else{
-        cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"),false)
-    }
-}
-
-const upload = multer({
-    storage,
-    fileFilter,
-    limits:{fileSize:99999999999,files:2}
+const s3 = new S3({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.ACCESS_SECRET_KEY,
 });
 
-router.post("/upload",upload.array("file"),async(req,res)=>{
-    try {
-        console.log(req.files)
-    const file =req.files[0];
-    console.log(file)
-    const result = await s3Uploadv2(file)
-    res.json({status:"success",result});
-    // console.log(result)
-    console.log(result.Location)
-    } catch (error) {
-        console.log(error)
+const ALLOWED_FILE_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/pjpeg",
+  "image/png",
+  "image/*",
+];
+
+const MAX_FILE_SIZE_ALLOWED = "10485760"; //10 MB
+const uploader = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.BUCKET,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, `${uuid()}-${file.originalname}`);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const fileSize = parseInt(req.headers["content-length"], 10);
+    if (ALLOWED_FILE_MIME_TYPES.indexOf(file.mimetype) === -1) {
+      return cb("NOT_A_VALID_FILE", false);
     }
+    if (fileSize > MAX_FILE_SIZE_ALLOWED) {
+      return cb("SIZE_EXCEEDS", false);
+    }
+    return cb(null, true);
+  },
+  limits: { fileSize: MAX_FILE_SIZE_ALLOWED, files: 2 },
 });
 
-router.use((error,req,res,next) => {
-    if(error instanceof multer.MulterError){
-        if(error.code === "LIMIT_FILE_SIZE"){
-            return res.json({message:"file is too large"})
+router.post("/upload", uploader.array("files"), async (req, res) => {
+  try {
+    console.log("req.files: ", req.files);
+    const uploadedImages = req.files.map((file) => {
+        return {
+            original: file.location
         }
-        if(error.code ==="LIMIT_FILE_COUNT"){
-            return res.json({message:"file limit is reached"})
-        }
-        if(error.code === "LIMIT_UNEXPECTED_FILE"){
-            return res.json({message:"file must be image"})
-        }
-    }
-})
+    });
+    res.json({ status: "success", data: uploadedImages });
+  } catch (error) {
+    console.log(error);
+    res.json({ message: "upload file error" });
+  }
+});
 
 module.exports = router;
