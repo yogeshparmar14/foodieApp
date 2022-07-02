25cf214ed90e1e1585f970f533dc89566ec97023
twointo2 <twointo2@gmail.com>
2022-07-03 00:57:49 +0530

    Addded Image uploaded way 2

--- src/modules/common/uploadFile.js
+++ src/modules/common/uploadFile.js
@@ -6,6 +6,7 @@ const { S3 } = require("aws-sdk");
 const uuid = require("uuid").v4;
 const router = express.Router();
 
+/* Way 1 */
 const s3 = new S3({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.ACCESS_SECRET_KEY,
@@ -48,9 +49,9 @@ router.post("/upload", uploader.array("files"), async (req, res) => {
   try {
     console.log("req.files: ", req.files);
     const uploadedImages = req.files.map((file) => {
-        return {
-            original: file.location
-        }
+      return {
+        original: file.location,
+      };
     });
     res.json({ status: "success", data: uploadedImages });
   } catch (error) {
@@ -58,5 +59,57 @@ router.post("/upload", uploader.array("files"), async (req, res) => {
     res.json({ message: "upload file error" });
   }
 });
+
+/* Way 2
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 99999999999, files: 2 },
+});
+
+router.post("/upload", upload.array("files"), async (req, res) => {
+  try {
+    let s3bucket = new S3({
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.ACCESS_SECRET_KEY,
+      Bucket: process.env.BUCKET,
+    });
+
+    const fileUploads = req.files.map(
+      (item) =>
+        new Promise((resolve, reject) => {
+          const params = {
+            Bucket: process.env.BUCKET,
+            Key: item.originalname,
+            Body: item.buffer,
+            ACL: "public-read",
+          };
+          s3bucket.upload(params, function (err, data) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        })
+    );
+    const fileUploadResult = await Promise.all(fileUploads);
+    res.json({ status: "success", result: fileUploadResult });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "failed", message: 'File uplaod failed. Please try again later' });
+  }
+});
+*/
 
 module.exports = router;
